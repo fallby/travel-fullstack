62ab230987cd53029fcaf335cbedf7d58897f995
fallby <t0304068@gmail.com>
2026-04-13 03:04:40 +0400

    startFrontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,31 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Countries from "./pages/Countries";
+import Tours from "./pages/Tours";
+import Login from "./pages/Login";
+
+import Nav from "./components/Nav";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  useEffect(() => {
-  fetch('/api/test')
-    .then(res => res.json())
-    .then(data => console.log(data));
-  }, []); 
+  // const [count, setCount] = useState(0)
+  // useEffect(() => {
+  // fetch('/api/test')
+  //   .then(res => res.json())
+  //   .then(data => console.log(data));
+  // }, []); 
 
   return (
-    <>
-      <section id="center">
-        
-      </section>
-    </>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/countries" element={<Countries />}/>
+        <Route path="/tours" element={<Tours />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/components/Nav.js
@@ -0,0 +1,11 @@
+export default function Nav() {
+    return (
+        <nav>
+            <Link to="/">Главная</Link>
+            <Link to="/countries">Страны</Link>
+            <Link to="/tours">Туры</Link>
+            <Link to="/login">Войти</Link>
+        </nav>
+    )
+}
+//https://metanit.com/web/react/4.3.php как выделить активную ссылку
